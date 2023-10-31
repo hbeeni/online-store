408ac7e41cecfc038f5461f81c00c2e57bba06e5
hbeeni <hyebeen.dev@gmail.com>
2023-11-01 04:39:44 +0900

    #6 - Enum을 사용해 상품, 주문, 배송 상태 정의

---
+++ src/main/java/com/been/onlinestore/domain/constant/OrderStatus.java
@@ -0,0 +1,17 @@
+package com.been.onlinestore.domain.constant;
+
+public enum OrderStatus {
+
+    ORDER("주문 완료"),
+    CANCEL("주문 취소");
+
+    private final String description;
+
+    OrderStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
