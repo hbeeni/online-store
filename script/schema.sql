d2b2bd9824d910d8ab4292ccb5d101a9539446ee
hbeeni <hyebeen.dev@gmail.com>
2023-11-02 02:40:48 +0900

    #8 - DB 테이블 생성 스크립트 파일 작성
    원래는 JPA에서 자동으로 생성해주는 DDL문 대신 이 스크립트를 사용하려고 했으나,
    향후 테이블이 바뀔 경우 수정하기 귀찮아질 거 같아서 사용하지 않기로 함.
    파일은 남겨 놓음.

---
+++ script/schema.sql
@@ -0,0 +1,149 @@
+set foreign_key_checks = 0;
+
+/* alter table address drop foreign key FK_address_users;
+alter table cart drop foreign key FK_cart_users;
+alter table cart_product drop foreign key FK_cart_product_cart;
+alter table cart_product drop foreign key FK_cart_product_product;
+alter table order_product drop foreign key FK_order_product_orders;
+alter table order_product drop foreign key FK_order_product_product;
+alter table orders drop foreign key FK_orders_delivery;
+alter table orders drop foreign key FK_orders_users;
+alter table product drop foreign key FK_product_category; */
+
+drop table if exists address;
+drop table if exists cart;
+drop table if exists cart_product;
+drop table if exists category;
+drop table if exists delivery;
+drop table if exists order_product;
+drop table if exists orders;
+drop table if exists product;
+drop table if exists users;
+
+set foreign_key_checks = 0;
+
+create table address (
+                         id bigint not null auto_increment,
+                         user_id bigint not null,
+                         detail varchar(50) not null,
+                         zipcode varchar(20) not null,
+                         default_address char(1) not null default 'N',
+                         created_at datetime(6) not null,
+                         modified_at datetime(6) not null,
+                         constraint PK_address PRIMARY KEY (id)
+) engine=InnoDB;
+
+create table cart (
+                      id bigint not null auto_increment,
+                      user_id bigint not null,
+                      constraint PK_cart PRIMARY KEY (id)
+) engine=InnoDB;
+
+
+create table cart_product (
+                              id bigint not null auto_increment,
+                              cart_id bigint not null,
+                              product_id bigint not null,
+                              product_price integer not null,
+                              product_quantity integer not null,
+                              constraint PK_cart_product PRIMARY KEY (id)
+) engine=InnoDB;
+
+create table category (
+                          id bigint not null auto_increment,
+                          name varchar(20) not null unique,
+                          description varchar(100),
+                          created_at datetime(6) not null,
+                          created_by varchar(50) not null,
+                          modified_at datetime(6) not null,
+                          modified_by varchar(50) not null,
+                          constraint PK_category PRIMARY KEY (id)
+) engine=InnoDB;
+
+create table delivery (
+                          id bigint not null auto_increment,
+                          delivery_status varchar(20) not null default 'ACCEPT',
+                          delivery_address varchar(50) not null,
+                          delivery_fee integer not null,
+                          receiver_phone varchar(20) not null,
+                          delivered_at datetime(6),
+                          constraint PK_delivery PRIMARY KEY (id)
+) engine=InnoDB;
+
+create table order_product (
+                               id bigint not null auto_increment,
+                               order_id bigint not null,
+                               product_id bigint not null,
+                               price integer not null,
+                               quantity integer not null,
+                               constraint PK_order_product PRIMARY KEY (id)
+) engine=InnoDB;
+
+create table orders (
+                        id bigint not null auto_increment,
+                        user_id bigint not null,
+                        delivery_id bigint not null,
+                        orderer_phone varchar(20) not null,
+                        order_status varchar(20) not null default 'ORDER',
+                        created_at datetime(6) not null,
+                        modified_at datetime(6) not null,
+                        constraint PK_orders PRIMARY KEY (id)
+) engine=InnoDB;
+
+create table product (
+                         id bigint not null auto_increment,
+                         category_id bigint not null,
+                         name varchar(100) not null,
+                         price integer not null,
+                         description varchar(255),
+                         stock_quantity integer not null,
+                         sales_volume integer not null default 0,
+                         sale_status varchar(20) not null default 'WAIT',
+                         image_url varchar(200),
+                         created_at datetime(6) not null,
+                         created_by varchar(50) not null,
+                         modified_at datetime(6) not null,
+                         modified_by varchar(50) not null,
+                         constraint PK_product PRIMARY KEY (id)
+) engine=InnoDB;
+
+create table users (
+                       id bigint not null auto_increment,
+                       uid varchar(50) not null,
+                       password varchar(255) not null,
+                       name varchar(20) not null,
+                       email varchar(100) not null,
+                       nickname varchar(20),
+                       phone varchar(20) not null,
+                       role_type varchar(20) not null,
+                       created_at datetime(6) not null,
+                       modified_at datetime(6) not null,
+                       constraint PK_users PRIMARY KEY (id)
+) engine=InnoDB;
+
+alter table address add constraint FK_address_users
+    foreign key (user_id) references users (id);
+
+alter table cart add constraint FK_cart_users
+    foreign key (user_id) references users (id);
+
+alter table cart_product add constraint FK_cart_product_cart
+    foreign key (cart_id) references cart (id);
+
+alter table cart_product add constraint FK_cart_product_product
+    foreign key (product_id) references product (id);
+
+alter table order_product add constraint FK_order_product_orders
+    foreign key (order_id) references orders (id);
+
+alter table order_product add constraint FK_order_product_product
+    foreign key (product_id) references product (id);
+
+alter table orders add constraint FK_orders_delivery
+    foreign key (delivery_id) references delivery (id);
+
+alter table orders add constraint FK_orders_users
+    foreign key (user_id) references users (id);
+
+alter table product add constraint FK_product_category
+    foreign key (category_id) references category (id);
