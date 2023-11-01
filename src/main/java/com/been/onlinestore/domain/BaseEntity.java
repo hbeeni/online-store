564c693792ee703ac2ae754b6dbc71eb85189dbc
hbeeni <hyebeen.dev@gmail.com>
2023-11-02 02:29:34 +0900

    #8 - 엔티티에 공통으로 들어가는 생성일시, 수정일시, 생성자, 수정자 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 엔티티 클래스에 들어가고,
    도메인과 직접 연관이 없는 요소이므로 추출이 가능함.
    생성일시, 수정일시는 단독으로 쓰이는 엔티티가 있어서 따로 만들어서 상속하는 방식을 사용함.
    
    '@MappedSuperclass'를 이용하여 상속 방식으로 추출함.
    그 외 '@DateTimeFormat' 요소를 추가함.

---
+++ src/main/java/com/been/onlinestore/domain/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.been.onlinestore.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 50)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 50)
+    private String modifiedBy;
+}
