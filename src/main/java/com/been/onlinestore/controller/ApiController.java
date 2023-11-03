7e346781b97f9844b7ceaff7d4f11b9c0cb335f6
hbeeni <hyebeen.dev@gmail.com>
2023-11-04 02:27:37 +0900

    #34 - 엔드포인트 수정
    * 모든 권한: /api -> /api/common
    * 권한 필요 없음: /api
    
    회원 정보 변경은 모든 권한이 공통으로 사용함.
    그래서 공통으로 빼고, `UserApiController`를 삭제함.
    이전에 만든 `CommonController`를 모든 권한이 공통으로 사용하게 하고,
    권한이 필요 없는 API를 `ApiController`로 수정함.
    
    위 변경점에 맞게 컨트롤러, 테스트를 수정함.

---
+++ src/main/java/com/been/onlinestore/controller/ApiController.java
@@ -0,0 +1,44 @@
+package com.been.onlinestore.controller;
+
+import com.been.onlinestore.controller.dto.ApiResponse;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class ApiController {
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<?> signUp() {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<?> getCategories() {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @GetMapping("/categories/{categoryId}/products")
+    public ResponseEntity<?> getAllProductsInCategory(@PathVariable Long categoryId, @RequestParam(required = false) Pageable pageable) {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<?> getNewArrivals() {
+        Pageable pageable = PageRequest.of(0, 9, Sort.by(Sort.Order.desc("createdAt")));
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<?> getProduct(@PathVariable Long productId) {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+}
--- src/main/java/com/been/onlinestore/controller/CommonController.java
+++ src/main/java/com/been/onlinestore/controller/CommonController.java
@@ -1,38 +1,17 @@
 package com.been.onlinestore.controller;
 
 import com.been.onlinestore.controller.dto.ApiResponse;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api")
+@RequestMapping("/api/common")
 @RestController
 public class CommonController {
 
-    @GetMapping("/categories")
-    public ResponseEntity<?> getCategories() {
-        return ResponseEntity.ok(ApiResponse.success(null));
-    }
-
-    @GetMapping("/categories/{categoryId}/products")
-    public ResponseEntity<?> getAllProductsInCategory(@PathVariable Long categoryId, @RequestParam(required = false) Pageable pageable) {
-        return ResponseEntity.ok(ApiResponse.success(null));
-    }
-
-    @GetMapping("/products")
-    public ResponseEntity<?> getNewArrivals() {
-        Pageable pageable = PageRequest.of(0, 9, Sort.by(Sort.Order.desc("createdAt")));
-        return ResponseEntity.ok(ApiResponse.success(null));
-    }
-
-    @GetMapping("/products/{productId}")
-    public ResponseEntity<?> getProduct(@PathVariable Long productId) {
+    @PutMapping("/info")
+    public ResponseEntity<?> updateUserInfo() {
         return ResponseEntity.ok(ApiResponse.success(null));
     }
 }
