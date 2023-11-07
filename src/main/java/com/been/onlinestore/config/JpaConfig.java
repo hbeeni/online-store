121896794f428125abe484a3b4afdae909cc8208
hbeeni <hyebeen.dev@gmail.com>
2023-11-08 03:01:25 +0900

    #44 - AuditorAware: 스프링 시큐리티 반영
    시큐리티 인증 정보에서 id를 가져와서
    엔티티의 작성자, 수정자에 반영되게 수정함.

--- src/main/java/com/been/onlinestore/config/JpaConfig.java
+++ src/main/java/com/been/onlinestore/config/JpaConfig.java
@@ -1,9 +1,13 @@
 package com.been.onlinestore.config;
 
+import com.been.onlinestore.controller.dto.security.PrincipalDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -13,6 +17,11 @@ public class JpaConfig {
     
     @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.of("been"); //TODO: 스프링 시큐리티로 인증 붙이면 수정
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(PrincipalDetails.class::cast)
+                .map(PrincipalDetails::getUsername);
     }
 }
