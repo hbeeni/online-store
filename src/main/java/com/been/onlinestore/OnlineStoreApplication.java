b599c67bef35a6a3ae339ab34e59c4ea5c18439f
hbeeni <hyebeen.dev@gmail.com>
2023-11-08 00:40:16 +0900

    #41 - 테스트 데이터 생성 및 프로퍼티 객체 빈 등록
    패스워드 인코딩을 위해 sql 스크립트 대신 프로그램 실행 시 테스트 데이터를 생성.
    `@EnableConfigurationProperties`로 jwt 프로퍼티 빈을 등록함.

--- src/main/java/com/been/onlinestore/OnlineStoreApplication.java
+++ src/main/java/com/been/onlinestore/OnlineStoreApplication.java
@@ -1,8 +1,17 @@
 package com.been.onlinestore;
 
+import com.been.onlinestore.config.jwt.JwtProperties;
+import com.been.onlinestore.domain.User;
+import com.been.onlinestore.domain.constant.RoleType;
+import com.been.onlinestore.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
 public class OnlineStoreApplication {
 
@@ -10,4 +19,11 @@ public class OnlineStoreApplication {
 		SpringApplication.run(OnlineStoreApplication.class, args);
 	}
 
+    @Bean
+    public CommandLineRunner loadData(UserRepository userRepository, PasswordEncoder encoder) {
+        return args -> {
+            userRepository.save(User.of("user", encoder.encode("test12"), "user", "testuser@mail.com", "test user", "01012341234"));
+            userRepository.save(User.of("admin", encoder.encode("test12"), "admin", "testadmin@mail.com", "test admin", "01012341234", RoleType.ADMIN));
+        };
+    }
 }
