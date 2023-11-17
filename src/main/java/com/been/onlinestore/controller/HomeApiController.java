73e88cc3f8179eecc5d055b6490d297b5997d934
hbeeni <hyebeen.dev@gmail.com>
2023-11-17 19:28:20 +0900

    #72 - 카테고리 컨트롤러 수정
    `HomeApiController`에 있던 카테고리 관련 메서드를 `CategoryApiController`로 이동함.
    이후 `HomeApiController`에 있는 메서드는 모두 옮긴 후 삭제할 예정.
    
    request 객체에 검증을 적용하고, 컨트롤러에도 검증 로직을 추가함.

--- src/main/java/com/been/onlinestore/controller/HomeApiController.java
+++ src/main/java/com/been/onlinestore/controller/HomeApiController.java
@@ -1,8 +1,6 @@
 package com.been.onlinestore.controller;
 
 import com.been.onlinestore.controller.dto.ApiResponse;
-import com.been.onlinestore.controller.dto.response.CategoryResponse;
-import com.been.onlinestore.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,30 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
 public class HomeApiController {
-
-    private final CategoryService categoryService;
-
-    @GetMapping("/categories")
-    public ResponseEntity<ApiResponse<List<CategoryResponse>>> getCategories() {
-        List<CategoryResponse> categoryResponses = categoryService.findCategories().stream()
-                .map(CategoryResponse::from)
-                .toList();
-        return ResponseEntity.ok(ApiResponse.success(categoryResponses));
-    }
-
-    @GetMapping("/categories/{categoryId}/products")
-    public ResponseEntity<?> getAllProductsInCategory(@PathVariable Long categoryId, @RequestParam(required = false) Pageable pageable) {
-        return ResponseEntity.ok(ApiResponse.success(null));
-    }
 
     @GetMapping({"/", "/products"})
     public ResponseEntity<?> getNewArrivals() {
--- src/test/java/com/been/onlinestore/controller/admin/AdminCategoryApiControllerTest.java
+++ src/test/java/com/been/onlinestore/controller/admin/AdminCategoryApiControllerTest.java
@@ -1,7 +1,9 @@
 package com.been.onlinestore.controller.admin;
 
 import com.been.onlinestore.config.TestSecurityConfig;
+import com.been.onlinestore.controller.dto.CategoryRequest;
 import com.been.onlinestore.service.CategoryService;
+import com.been.onlinestore.service.response.admin.AdminCategoryResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,10 +12,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static com.been.onlinestore.util.CategoryTestDataUtil.createCategoryDto;
+import java.util.List;
+
+import static com.been.onlinestore.util.CategoryTestDataUtil.createAdminCategoryResponse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -25,9 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@Disabled("구현 전")
 @DisplayName("API 컨트롤러 - 카테고리 (관리자)")
-@ActiveProfiles("test")
 @Import(TestSecurityConfig.class)
 @WebMvcTest(AdminCategoryApiController.class)
 class AdminCategoryApiControllerTest {
@@ -37,33 +38,61 @@ class AdminCategoryApiControllerTest {
 
     @MockBean private CategoryService categoryService;
 
-    private static final long ID = 1L;
+    @DisplayName("[API][GET] 카테고리 조회")
+    @Test
+    void test_getCategories() throws Exception {
+        //Given
+        long id = 1L;
+        AdminCategoryResponse response = createAdminCategoryResponse(id, "test category", 1);
+
+        given(categoryService.findCategoriesForAdmin()).willReturn(List.of(response));
+
+        //When & Then
+        mvc.perform(get("/api/admin/categories"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value("success"))
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data[0].id").value(response.id()))
+                .andExpect(jsonPath("$.data[0].name").value(response.name()))
+                .andExpect(jsonPath("$.data[0].productCount").value(response.productCount()))
+                .andExpect(jsonPath("$.data[0].createdBy").value(response.createdBy()))
+                .andExpect(jsonPath("$.data[0].modifiedBy").value(response.modifiedBy()));
+        then(categoryService).should().findCategoriesForAdmin();
+    }
 
     @DisplayName("[API][GET] 카테고리 상세 조회")
     @Test
     void test_getCategory() throws Exception {
         //Given
-        String name = "test category";
-        int productCount = 1;
-        given(categoryService.findCategory(any()))
-                .willReturn(createCategoryDto(ID, name, productCount));
+        long id = 1L;
+        AdminCategoryResponse response = createAdminCategoryResponse(id, "test category", 1);
+
+        given(categoryService.findCategory(id)).willReturn(response);
 
         //When & Then
-        mvc.perform(get("/api/admin/categories/" + ID))
+        mvc.perform(get("/api/admin/categories/" + id))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.status").value("success"))
-                .andExpect(jsonPath("$.data.id").value(ID))
-                .andExpect(jsonPath("$.data.name").value(name))
-                .andExpect(jsonPath("$.data.productCount").value(productCount));
-        then(categoryService).should().findCategory(any());
+                .andExpect(jsonPath("$.data.id").value(response.id()))
+                .andExpect(jsonPath("$.data.name").value(response.name()))
+                .andExpect(jsonPath("$.data.productCount").value(response.productCount()))
+                .andExpect(jsonPath("$.data.createdBy").value(response.createdBy()))
+                .andExpect(jsonPath("$.data.modifiedBy").value(response.modifiedBy()));
+        then(categoryService).should().findCategory(id);
     }
 
     @DisplayName("[API][POST] 카테고리 추가")
     @Test
     void test_addCategory() throws Exception {
         //Given
-        given(categoryService.addCategory(any())).willReturn(ID);
+        long id = 1L;
+        CategoryRequest.Create request = CategoryRequest.Create.builder()
+                .name("상의")
+                .description("상의입니다.")
+                .build();
+        given(categoryService.addCategory(request.toServiceRequest())).willReturn(id);
 
         //When & Then
         mvc.perform(
@@ -71,59 +100,54 @@ class AdminCategoryApiControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON)
                                 .characterEncoding("UTF-8")
-                                .content("""
-                                {
-                                    "name": "category",
-                                    "description": "description"
-                                }
-                                """)
+                                .content(mapper.writeValueAsString(request))
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.status").value("success"))
-                .andExpect(jsonPath("$.data.id").value(ID));
-        then(categoryService).should().addCategory(any());
+                .andExpect(jsonPath("$.data.id").value(id));
+        then(categoryService).should().addCategory(request.toServiceRequest());
     }
 
     @DisplayName("[API][PUT] 카테고리 수정")
     @Test
     void test_updateCategory() throws Exception {
         //Given
+        long id = 1L;
         String name = "updated name";
-        given(categoryService.updateCategory(any(), any())).willReturn(ID);
+        CategoryRequest.Update request = CategoryRequest.Update.builder()
+                .name(name)
+                .build();
+        given(categoryService.updateCategory(id, request.toServiceRequest())).willReturn(id);
 
         //When & Then
         mvc.perform(
-                        put("/api/admin/categories/" + ID)
+                        put("/api/admin/categories/" + id)
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON)
                                 .characterEncoding("UTF-8")
-                                .content("""
-                                        {
-                                            "name": "updated name",
-                                            "description": "updated name description"
-                                        }
-                                        """)
+                                .content(mapper.writeValueAsString(request))
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.status").value("success"))
-                .andExpect(jsonPath("$.data.id").value(ID));
-        then(categoryService).should().updateCategory(any(), any());
+                .andExpect(jsonPath("$.data.id").value(id));
+        then(categoryService).should().updateCategory(id, request.toServiceRequest());
     }
 
     @DisplayName("[API][DELETE] 카테고리 삭제")
     @Test
     void test_deleteCategory() throws Exception {
         //Given
-        given(categoryService.deleteCategory(any())).willReturn(ID);
+        long id = 1L;
+        given(categoryService.deleteCategory(id)).willReturn(id);
 
         //When & Then
-        mvc.perform(delete("/api/admin/categories/" + ID))
+        mvc.perform(delete("/api/admin/categories/" + id))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.status").value("success"))
-                .andExpect(jsonPath("$.data.id").value(ID));
+                .andExpect(jsonPath("$.data.id").value(id));
         then(categoryService).should().deleteCategory(any());
     }
 }
