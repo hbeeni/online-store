e16bc55ab17e3ed98868468dfdd1834a0a43a259
hbeeni <hyebeen.dev@gmail.com>
2023-11-17 19:25:21 +0900

    #72 - 카테고리 서비스, 상품 서비스 수정
    * 카테고리 서비스: 카테고리 정보를 반환하는 메서드를 어드민용, 일반 회원용 따로 만듦
    * 상품 서비스: 해당 카테고리의 상품을 조회하는 메서드 생성

--- src/test/java/com/been/onlinestore/service/CategoryServiceTest.java
+++ src/test/java/com/been/onlinestore/service/CategoryServiceTest.java
@@ -1,9 +1,11 @@
 package com.been.onlinestore.service;
 
 import com.been.onlinestore.domain.Category;
-import com.been.onlinestore.dto.CategoryDto;
 import com.been.onlinestore.repository.CategoryRepository;
 import com.been.onlinestore.repository.ProductRepository;
+import com.been.onlinestore.service.request.CategoryServiceRequest;
+import com.been.onlinestore.service.response.CategoryResponse;
+import com.been.onlinestore.service.response.admin.AdminCategoryResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,8 +17,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.been.onlinestore.util.CategoryTestDataUtil.createCategory;
-import static com.been.onlinestore.util.CategoryTestDataUtil.createCategoryDto;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -31,42 +33,65 @@ class CategoryServiceTest {
 
     @InjectMocks private CategoryService sut;
 
-    @DisplayName("모든 카테고리 정보를 반환한다.")
+    @DisplayName("[어드민] 모든 카테고리 정보를 반환한다.")
     @Test
-    void test_searchCategories() {
+    void test_findCategoriesForAdmin() {
         //Given
-        given(categoryRepository.findAllWithProducts()).willReturn(List.of(
+        given(categoryRepository.findAll()).willReturn(List.of(
                 createCategory("상의"),
                 createCategory("하의"),
                 createCategory("음식")
         ));
 
         //When
-        List<CategoryDto> result = sut.findCategories();
+        List<AdminCategoryResponse> result = sut.findCategoriesForAdmin();
 
         //Then
-        assertThat(result.size()).isEqualTo(3);
-        then(categoryRepository).should().findAllWithProducts();
+        assertThat(result).hasSize(3)
+                .first()
+                .hasFieldOrProperty("createdAt")
+                .hasFieldOrProperty("createdBy")
+                .hasFieldOrProperty("modifiedAt")
+                .hasFieldOrProperty("modifiedBy");
+        then(categoryRepository).should().findAll();
+    }
+
+    @DisplayName("[일반회원] 판매 중인 상품이 존재하는 모든 카테고리 정보를 반환한다.")
+    @Test
+    void test_findCategoriesForUser() {
+        //Given
+        given(categoryRepository.findAll()).willReturn(List.of(
+                createCategory("상의"),
+                createCategory("하의"),
+                createCategory("음식")
+        ));
+
+        //When
+        List<CategoryResponse> result = sut.findCategoriesForUser();
+
+        //Then
+        assertThat(result.size()).isEqualTo(0);
+        then(categoryRepository).should().findAll();
     }
 
     @DisplayName("카테고리를 조회하면, 카테고리 정보를 반환한다.")
     @Test
-    void test_searchCategory() {
+    void test_findCategory() {
         //Given
         long id = 1L;
-        int productCount = 0;
         Category category = createCategory("카테고리");
-        given(categoryRepository.findWithProductsById(any())).willReturn(Optional.of(category));
+        given(categoryRepository.findById(id)).willReturn(Optional.of(category));
 
         //When
-        CategoryDto result = sut.findCategory(id);
+        AdminCategoryResponse result = sut.findCategory(id);
 
         //Then
         assertThat(result)
                 .hasFieldOrPropertyWithValue("id", category.getId())
                 .hasFieldOrPropertyWithValue("name", category.getName())
-                .hasFieldOrPropertyWithValue("productCount", productCount);
-        then(categoryRepository).should().findWithProductsById(any());
+                .hasFieldOrProperty("productCount")
+                .hasFieldOrProperty("createdAt");
+        then(categoryRepository).should().findById(id);
     }
 
     @DisplayName("카테고리를 추가하면, 저장된 카테고리의 id를 반환한다.")
@@ -77,7 +102,7 @@ class CategoryServiceTest {
         given(categoryRepository.save(any())).willReturn(createCategory(name));
 
         //When
-        Long result = sut.addCategory(createCategoryDto(name));
+        Long result = sut.addCategory(CategoryServiceRequest.Create.of(name, name));
 
         //Then
         assertThat(result).isNotNull();
@@ -90,16 +115,30 @@ class CategoryServiceTest {
         //Given
         long id = 1L;
         Category category = createCategory(id, "category");
-        CategoryDto dto = createCategoryDto(id, "update category");
-        given(categoryRepository.getReferenceById(id)).willReturn(category);
+        CategoryServiceRequest.Update serviceRequest = CategoryServiceRequest.Update.of("update category", "update cateogory");
+        given(categoryRepository.findById(id)).willReturn(Optional.of(category));
 
         //When
-        Long result = sut.updateCategory(id, dto);
+        Long result = sut.updateCategory(id, serviceRequest);
 
         //Then
         assertThat(result).isEqualTo(id);
-        assertThat(category).hasFieldOrPropertyWithValue("name", dto.name());
-        then(categoryRepository).should().getReferenceById(id);
+        assertThat(category).hasFieldOrPropertyWithValue("name", serviceRequest.name());
+        then(categoryRepository).should().findById(id);
+    }
+
+    @DisplayName("존재하지 않는 카테고리 정보를 수정하려고 하면, 예외를 던진다.")
+    @Test
+    void test_updateCategory_withNonexistentId_throwsIllegalException() {
+        //Given
+        long id = 1L;
+        CategoryServiceRequest.Update serviceRequest = CategoryServiceRequest.Update.of("update category", "update cateogory");
+        given(categoryRepository.findById(id)).willReturn(Optional.empty());
+
+        //When & Then
+        assertThatThrownBy(() -> sut.updateCategory(id, serviceRequest))
+                .isInstanceOf(IllegalArgumentException.class);
+        then(categoryRepository).should().findById(id);
     }
 
     @DisplayName("카테고리를 삭제하면, 삭제된 카테고리의 id를 반환한다.")
