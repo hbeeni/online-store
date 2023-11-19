1dc3cd8caf9a7631f53b7f85a5789393977803ea
hbeeni <hyebeen.dev@gmail.com>
2023-11-20 01:44:43 +0900

    #77 - UserRequest: 검증 추가
    패턴(정규식)을 사용해서 좀 더 세밀하게 검증을 함.

--- src/main/java/com/been/onlinestore/controller/dto/UserRequest.java
+++ src/main/java/com/been/onlinestore/controller/dto/UserRequest.java
@@ -4,16 +4,44 @@ import com.been.onlinestore.domain.constant.RoleType;
 import com.been.onlinestore.service.request.UserServiceRequest;
 import lombok.Builder;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public record UserRequest() {
+
+    private static final String UID_PATTERN_REGEX = "^[a-z]{1}[a-z0-9]{3,9}$";
+    private static final String UID_PATTERN_MESSAGE = "영문 또는 영문 + 숫자 조합 4 ~ 10자리를 입력해주세요.";
+
+    private static final String PHONE_PATTERN_REGEX = "^010([0-9]{7,8})+$";
+    private static final String PHONE_PATTERN_MESSAGE = "'-'(하이픈) 없이 10 ~ 11 자리의 숫자만 입력 가능합니다.";
+
+    private static final String EMAIL_PATTERN_REGEX = "^[a-zA-Z0-9]+@[0-9a-zA-Z]+\\.[a-z]{3}+$";
+    private static final String EMAIL_PATTERN_MESSAGE = "잘못된 이메일 형식입니다.";
 
     @Builder
     public record SignUp(
+            @NotBlank @Size(max = 50)
+            @Pattern(regexp = UID_PATTERN_REGEX, message = UID_PATTERN_MESSAGE)
             String uid,
+
+            @NotBlank @Size(max = 255)
             String password,
+
+            @NotBlank @Size(max = 20)
             String name,
+
+            @NotBlank @Size(max = 100)
+            @Pattern(regexp = EMAIL_PATTERN_REGEX, message = EMAIL_PATTERN_MESSAGE)
             String email,
+
+            @Size(max = 20)
             String nickname,
+
+            @NotBlank @Size(max = 20)
+            @Pattern(regexp = PHONE_PATTERN_REGEX, message = PHONE_PATTERN_MESSAGE)
             String phone,
+
             RoleType roleType
     ) {
 
@@ -36,7 +64,21 @@ public record UserRequest() {
 
     @Builder
     public record Login(
+            @NotBlank @Size(max = 50)
+            @Pattern(regexp = UID_PATTERN_REGEX, message = UID_PATTERN_MESSAGE)
             String uid,
+
+            @NotBlank @Size(max = 255)
             String password
+    ) {}
+
+    @Builder
+    public record Update(
+            @Size(max = 20)
+            String nickname,
+
+            @NotBlank @Size(max = 20)
+            @Pattern(regexp = PHONE_PATTERN_REGEX, message = PHONE_PATTERN_MESSAGE)
+            String phone
     ) {}
 }
