113459128ef2ddea9c7dbffe1a1e8ae1dd3729c2
hbeeni <hyebeen.dev@gmail.com>
2023-11-21 04:33:14 +0900

    #79 - 장바구니 상품 request 검증 및 ServiceRequest 변환 메서드 추가

--- src/main/java/com/been/onlinestore/controller/dto/CartProductRequest.java
+++ src/main/java/com/been/onlinestore/controller/dto/CartProductRequest.java
@@ -1,17 +1,33 @@
 package com.been.onlinestore.controller.dto;
 
+import com.been.onlinestore.service.request.CartProductServiceRequest;
 import lombok.Builder;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 public record CartProductRequest() {
 
     @Builder
     public record Create(
+            @NotNull @Min(1)
             Long productId,
+
+            @NotNull @Min(1)
             Integer productQuantity
-    ) {}
+    ) {
+
+        public CartProductServiceRequest.Create toServiceRequest() {
+            return CartProductServiceRequest.Create.of(
+                    productId,
+                    productQuantity
+            );
+        }
+    }
 
     @Builder
     public record Update(
+            @NotNull @Min(1)
             Integer productQuantity
     ) {}
 }
