7368c67db80495626ae1204c4567a8398a54c916
Hyebeen <hyebeen.dev@gmail.com>
2023-12-02 01:13:41 +0900

    #101 - ExceptionControllerAdvice 수정: 원인 예외가 있는 경우 해당 예외의 메시지를 출력
    원인 예외가 없는 경우 `NPE`가 발생하는 것을 발견하고,
    원인 예외가 있는 경우에 원인 예외의 메시지를 출력하게 해당 부분을 수정함.

--- src/main/java/com/been/onlinestore/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/been/onlinestore/exception/ExceptionControllerAdvice.java
@@ -57,13 +57,15 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiErrorResponse<Void>> runtimeEx(RuntimeException e) {
-        log.error(e.getCause().getMessage());
+        Throwable t = e.getCause() == null ? e : e.getCause();
+        log.error(t.getMessage());
         return ResponseEntity.badRequest().body(ApiErrorResponse.fail(e));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse<Void>> internalServerError(Exception e) {
-        log.error(e.getCause().getMessage());
+        Throwable t = e.getCause() == null ? e : e.getCause();
+        log.error(t.getMessage());
         return ResponseEntity.internalServerError().body(ApiErrorResponse.error(e));
     }
 }
