b52c61f7845de279da3fbab9c3eada8ee13dd9cf
Hyebeen <hyebeen.dev@gmail.com>
2023-12-18 06:34:17 +0900

    #133 - 회원 정보 조회 기능 추가
    회원이 자신의 정보를 조회할 수 있어야 하는데 누락된 걸 발견함.
    이제 추가해줌.

--- src/main/java/com/been/onlinestore/controller/UserApiController.java
+++ src/main/java/com/been/onlinestore/controller/UserApiController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.been.onlinestore.controller.dto.ApiResponse;
 import com.been.onlinestore.controller.dto.UserRequest;
 import com.been.onlinestore.controller.dto.security.PrincipalDetails;
 import com.been.onlinestore.service.UserService;
+import com.been.onlinestore.service.dto.response.UserResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +25,13 @@ import lombok.RequiredArgsConstructor;
 public class UserApiController {
 
 	private final UserService userService;
+
+	@GetMapping("/info")
+	public ResponseEntity<ApiResponse<UserResponse>> getUserInfo(
+		@AuthenticationPrincipal PrincipalDetails principalDetails
+	) {
+		return ResponseEntity.ok(ApiResponse.success(userService.findUser(principalDetails.id())));
+	}
 
 	@PutMapping("/info")
 	public ResponseEntity<ApiResponse<Map<String, Long>>> updateUserInfo(
--- src/test/java/com/been/onlinestore/controller/UserApiControllerTest.java
+++ src/test/java/com/been/onlinestore/controller/UserApiControllerTest.java
@@ -3,11 +3,14 @@ package com.been.onlinestore.controller;
 import static com.been.onlinestore.controller.restdocs.FieldDescription.*;
 import static com.been.onlinestore.controller.restdocs.RestDocsUtils.*;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.*;
+import static java.time.LocalDateTime.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,7 +25,9 @@ import com.been.onlinestore.config.TestSecurityConfig;
 import com.been.onlinestore.controller.dto.UserRequest;
 import com.been.onlinestore.controller.restdocs.RestDocsSupport;
 import com.been.onlinestore.controller.restdocs.TagDescription;
+import com.been.onlinestore.domain.constant.RoleType;
 import com.been.onlinestore.service.UserService;
+import com.been.onlinestore.service.dto.response.UserResponse;
 
 @DisplayName("API 컨트롤러 - 회원")
 @Import(TestSecurityConfig.class)
@@ -31,6 +36,67 @@ class UserApiControllerTest extends RestDocsSupport {
 
 	@MockBean
 	UserService userService;
+
+	@WithUserDetails
+	@DisplayName("[API][GET] 회원 상세 조회")
+	@Test
+	void test_getUser() throws Exception {
+		//Given
+		long id = 1L;
+		String uid = "user1";
+		UserResponse response = UserResponse.of(
+			id,
+			uid,
+			"$2a$10$wcVfFiEQnqu3WjgyiIsPzuqdYKV9WJ08Wx.4aac0e08CLFpUjvoW6",
+			"user1",
+			uid + "@mail.com",
+			"test user",
+			"01012345678",
+			RoleType.USER,
+			LocalDateTime.now().minusDays(3),
+			now()
+		);
+
+		given(userService.findUser(id)).willReturn(response);
+
+		//When & Then
+		mvc.perform(get("/api/users/info"))
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.status").value("success"))
+			.andExpect(jsonPath("$.data.id").value(response.id()))
+			.andExpect(jsonPath("$.data.uid").value(response.uid()))
+			.andExpect(jsonPath("$.data.name").value(response.name()))
+			.andExpect(jsonPath("$.data.password").doesNotExist())
+			.andDo(document(
+				"user/user/getUser",
+				userApiDescription(TagDescription.USER, "회원 상세 조회"),
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				responseFields(
+					STATUS,
+					fieldWithPath("data.id").type(JsonFieldType.NUMBER)
+						.description(USER_ID.getDescription()),
+					fieldWithPath("data.uid").type(JsonFieldType.STRING)
+						.description(USER_UID.getDescription()),
+					fieldWithPath("data.name").type(JsonFieldType.STRING)
+						.description(USER_NAME.getDescription()),
+					fieldWithPath("data.email").type(JsonFieldType.STRING)
+						.description(USER_EMAIL.getDescription()),
+					fieldWithPath("data.nickname").type(JsonFieldType.STRING)
+						.description(USER_NICKNAME.getDescription()),
+					fieldWithPath("data.phone").type(JsonFieldType.STRING)
+						.description(USER_PHONE.getDescription()),
+					fieldWithPath("data.roleType").type(JsonFieldType.STRING)
+						.description(USER_ROLE_TYPE.getDescription()),
+					fieldWithPath("data.createdAt").type(JsonFieldType.STRING)
+						.description(USER_CREATED_AT.getDescription()),
+					fieldWithPath("data.modifiedAt").type(JsonFieldType.STRING)
+						.description(USER_MODIFIED_AT.getDescription())
+				)
+			));
+		then(userService).should().findUser(id);
+	}
 
 	@WithUserDetails
 	@DisplayName("[API][PUT] 회원 정보 변경 - 닉네임과 휴대폰 번호 변경 가능")
