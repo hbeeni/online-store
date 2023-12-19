d0ddf78d3938eb62a1075ff7fd6e283be62e4287
Hyebeen <hyebeen.dev@gmail.com>
2023-12-20 01:25:32 +0900

    #144 - 30일 전의 장바구니 상품은 DB에서 삭제하는 로직 추가
    매일 자정 마지막으로 수정된 지 30일이 지난 장바구니 상품을 삭제하는 스케쥴러를 설정함.

--- src/main/java/com/been/onlinestore/OnlineStoreApplication.java
+++ src/main/java/com/been/onlinestore/OnlineStoreApplication.java
@@ -3,9 +3,11 @@ package com.been.onlinestore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.been.onlinestore.config.jwt.JwtProperties;
 
+@EnableScheduling
 @EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
 public class OnlineStoreApplication {
