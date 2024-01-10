f5c86ce9c6e404d54310e44b0b3df292e579531d
Hyebeen <hyebeen.dev@gmail.com>
2024-01-10 18:36:27 +0900

    #164 - 주문 테스트 시 사용할 static 메서드 추가

--- src/test/java/com/been/onlinestore/util/OrderTestDataUtil.java
+++ src/test/java/com/been/onlinestore/util/OrderTestDataUtil.java
@@ -4,8 +4,10 @@ import static com.been.onlinestore.util.UserTestDataUtil.*;
 
 import org.springframework.test.util.ReflectionTestUtils;
 
+import com.been.onlinestore.domain.Delivery;
 import com.been.onlinestore.domain.DeliveryRequest;
 import com.been.onlinestore.domain.Order;
+import com.been.onlinestore.domain.constant.DeliveryStatus;
 import com.been.onlinestore.domain.constant.OrderStatus;
 
 public class OrderTestDataUtil {
@@ -19,6 +21,22 @@ public class OrderTestDataUtil {
 			3000
 		);
 		ReflectionTestUtils.setField(order, "id", id);
+		return order;
+	}
+
+	public static Order createOrder(Long id, DeliveryStatus deliveryStatus) {
+		Order order = Order.create(
+			createUser(),
+			DeliveryRequest.of("delivery address", "receiver name", "01012341234"),
+			"01012341234",
+			OrderStatus.ORDER,
+			3000
+		);
+
+		Delivery delivery = Delivery.of(deliveryStatus, 3000, null);
+
+		ReflectionTestUtils.setField(order, "id", id);
+		ReflectionTestUtils.setField(order, "delivery", delivery);
 		return order;
 	}
 }
